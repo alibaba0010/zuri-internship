ca053b586bde0a0867d70471230d3aee492da365
alibaba0010 <zakariyahali100@gmail.com>
2023-09-12 09:29:26 +0100

    commit

--- src/user.js
+++ src/user.js
@@ -1,29 +1,45 @@
 import User from "./mongo.js";
 export const createUser = async (req, res) => {
   const { name, password } = req.body;
-  if (!name || !password) throw new Error("Please provide name and password");
-  const user = await User.create({ name, password });
-  res.status(201).json({ user: user.name });
+  try {
+    if (!name || !password) throw new Error("Please provide name and password");
+    const user = await User.create({ name, password });
+    res.status(201).json({ user: { name: user.name, id: user.id } });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
 };
 export const updateUser = async (req, res) => {
   const { name } = req.body;
   const { user_id } = req.params;
-  if (!name) throw new Error("Please provide name");
-  const user = await User.findById(user_id);
-  user.name = name;
-  const updateUser = await user.save();
-  res.status(200).json({ user: updateUser.name });
+  try {
+    if (!name) throw new Error("Please provide name");
+    const user = await User.findById(user_id);
+    user.name = name;
+    const updateUser = await user.save();
+    res.status(200).json({ user: updateUser.name });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
 };
 export const deleteUser = async (req, res) => {
   const { user_id } = req.params;
   const user = await User.findById(user_id);
-  if (!user) throw new Error("User not found");
-  await user.remove();
-  res.status(200).json({ message: "User deleted" });
+  try {
+    if (!user) throw new Error("User not found");
+    await user.deleteOne();
+    res.status(200).json({ message: "User deleted" });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
 };
 export const getUser = async (req, res) => {
-  const { user_id } = req.params;
-  const user = await User.findById(user_id);
-  if (!user) throw new Error("User not found");
-  res.status(200).json({ user: user.name });
+  try {
+    const { user_id } = req.params;
+    const user = await User.findById(user_id);
+    if (!user) throw new Error("User not found");
+    res.status(200).json({ user: user.name });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
 };
